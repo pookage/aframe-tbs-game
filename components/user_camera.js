acb42bda5a3544776fc9d2aeabe574cc98a1e6d9
pookage <pookagehayes@gmail.com>
2017-10-15 21:15:58 +0100

    added keyboard rotation to the camera

--- components/user_camera.js
+++ components/user_camera.js
@@ -1,5 +1,6 @@
 AFRAME.registerPrimitive("a-user-camera", {
 	defaultComponents: {
+		user_camera: {},
 		camera: {
 			fov: 60
 		},
@@ -23,3 +24,43 @@ AFRAME.registerPrimitive("a-user-camera", {
 		}
 	}
 });
+AFRAME.registerComponent("user_camera", {
+	schema: {
+		rotate_speed: {
+			default: 1
+		}
+	},
+	init: function(){
+
+		this.rotateDirection = 0;
+
+		window.addEventListener("keydown", this.lookupKey.bind(this));
+		window.addEventListener("keyup", this.stopRotation.bind(this));
+	},
+	tick: function(){
+		if(!!this.rotateDirection){
+			this.currentRotation = this.el.getAttribute("rotation");
+			if(this.rotateDirection > 0){
+				this.el.setAttribute("rotation", `${this.currentRotation.x} ${this.currentRotation.y += this.data.rotate_speed}`);
+			} else if (this.rotateDirection < 0){
+				this.el.setAttribute("rotation", `${this.currentRotation.x} ${this.currentRotation.y -= this.data.rotate_speed}`);
+			}
+		}
+	},
+	lookupKey: function(event){
+		switch(event.key){
+			case "e":
+				this.rotate(-1);
+				break;
+			case "q":
+				this.rotate(1);
+				break
+		}
+	},
+	stopRotation: function(event){
+		this.rotate(0);
+	},
+	rotate: function(direction){
+		this.rotateDirection = direction;
+	}
+})
